dd57d5c346ae655b829b3389b526710673000758
Damia Dembicki <nikeid1235@gmail.com>
2021-03-22 23:45:54 +0100

    Added Read Later functionality using localStorage

--- src/main.js
+++ src/main.js
@@ -6,49 +6,80 @@ import './styles/main.css';
 const newsListElement = document.querySelector('.newsList');
 const sectionSelectElement = document.querySelector('#sectionSelect');
 const activePageSelectElement = document.querySelector('#activePageSelect');
-// const readLaterListElement = document.querySelector('.readLaterList');
-// const readLaterButton = document.querySelector('.readLaterButton');
+const readLaterListElement = document.querySelector('.readLaterList');
 
 let currSection = 'search';
 let currPage = 1;
-// const readLaterArr = [{}];
 
 const today = new Date(); //today's date
 const startDate = new Date(new Date().setDate(today.getDate() - 30))
-  .toISOString()
-  .slice(0, 10); //30 days ago
+    .toISOString()
+    .slice(0, 10); //30 days ago
 const endDate = today.toISOString().slice(0, 10);
 
-// const sampleArray = [];
-// sampleArray.push(sampleData, sampleData2);
+const saveToLocalStorage = (e, items) => {
+    const item = items[e.target.dataset.index];
+    const { id } = item;
+    const jsonObj = JSON.stringify(item);
+    localStorage.setItem(id, jsonObj);
+    readFromLocalStorage();
+};
 
-// const jsonObj = JSON.stringify(sampleArray);
+const readFromLocalStorage = () => {
+    let values = [];
+    let keys = Object.keys(localStorage);
+    let i = keys.length;
+    while (i--) {
+        values.push(JSON.parse(localStorage.getItem(keys[i])));
+    }
+    const toRender = values
+        ?.map(({ webTitle, webUrl, id }) => {
+            return `
+        <li class="readLaterItem>
+        <h4 class="readLaterItem-title">${webTitle}</h4>
+        <section>
+          <a href="${webUrl}" target="_blank" class="button button-clear">Read</a>
+          <button class="button button-clear remove-button" data-id="${id}">Remove</button>
+        </section>
+      </li>
+            `;
+        })
+        .join('');
+    readLaterListElement.innerHTML = toRender;
+    const removeButtons = document.querySelectorAll('.remove-button');
+    removeButtons.forEach((button) =>
+        button.addEventListener('click', (e) => removeFromLocalStorage(e))
+    );
+};
 
-// // localStorage.setItem("NewsArray", jsonObj);
-// let lookUp = localStorage.getItem("NewsArray");
-// console.log(JSON.parse(lookUp));
+const removeFromLocalStorage = (e) => {
+    const itemId = e.target.dataset.id;
+    localStorage.removeItem(itemId);
+    readFromLocalStorage();
+};
 
 const loadData = async (section, page) => {
-  const API_KEY = '7eb760ef-b13f-409e-a7d0-0a7226c8356c';
-  const URL = 'https://content.guardianapis.com';
-  const slug = section ? section : 'search';
-  const pageNr = page ? page : 1;
+    const API_KEY = '7eb760ef-b13f-409e-a7d0-0a7226c8356c';
+    const URL = 'https://content.guardianapis.com';
+    const slug = section ? section : 'search';
+    const pageNr = page ? page : 1;
 
-  const response = await fetch(
-    `${URL}/${slug}?api-key=${API_KEY}&page=${pageNr}&from-date=${startDate}&to-date=${endDate}`
-  );
-  const json = await response.json();
-  console.log(json);
-  return json;
+    const response = await fetch(
+        `${URL}/${slug}?api-key=${API_KEY}&page=${pageNr}&from-date=${startDate}&to-date=${endDate}`
+    );
+    const json = await response.json();
+    console.log(json);
+    return json;
 };
 
 const renderContent = (json) => {
-  const PageAmount = json.response.pages;
-  const pages = [{}];
-  const news = json.response.results
-    ?.map(({ webTitle, sectionName, webPublicationDate, webUrl }) => {
-      const date = new Date(webPublicationDate).toLocaleDateString('pl-PL');
-      return `
+    const PageAmount = json.response.pages;
+    const pages = [{}];
+    const items = json.response.results;
+    const news = items
+        ?.map(({ webTitle, sectionName, webPublicationDate, webUrl }, index) => {
+            const date = new Date(webPublicationDate).toLocaleDateString('pl-PL');
+            return `
                 <li>
                     <article class="news">
                     <header>
@@ -62,39 +93,42 @@ const renderContent = (json) => {
                     </section>
                     <section class="newsActions">
                         <a href="${webUrl}" target="_blank" class="button">Full article</a>
-                        <button class="button button-outline readLaterButton">Read Later</button>
+                        <button class="button button-outline save-button" data-index="${index}">Read Later</button>
                     </section>
                     </article>
                 </li>
                 `;
-    })
-    .join('');
+        })
+        .join('');
 
-  for (let i = 1; i <= PageAmount; i++) {
-    pages.push(`<option value="${i}">${i}</option>`);
-  }
+    for (let i = 1; i <= PageAmount; i++) {
+        pages.push(`<option value="${i}">${i}</option>`);
+    }
 
-  activePageSelectElement.innerHTML = pages;
-  activePageSelectElement.value = currPage;
-  newsListElement.innerHTML = news;
+    //render elements
+    activePageSelectElement.innerHTML = pages;
+    activePageSelectElement.value = currPage;
+    newsListElement.innerHTML = news;
+    //add funtions to read later buttons
+    const saveButtons = document.querySelectorAll('.save-button');
+    saveButtons.forEach((button) =>
+        button.addEventListener('click', (e) => saveToLocalStorage(e, items))
+    );
 };
 
 //initial load data
 loadData(currSection).then((data) => renderContent(data));
+readFromLocalStorage();
 
 //event listeners
 sectionSelectElement?.addEventListener('change', (e) => {
-  currSection = e.target.value.toLowerCase();
-  if (currSection === 'all') currSection = 'search';
-  currPage = 1;
-  loadData(currSection, currPage).then((data) => renderContent(data));
+    currSection = e.target.value.toLowerCase();
+    if (currSection === 'all') currSection = 'search';
+    currPage = 1;
+    loadData(currSection, currPage).then((data) => renderContent(data));
 });
 
 activePageSelectElement?.addEventListener('change', (e) => {
-  currPage = e.target.value;
-  loadData(currSection, currPage).then((data) => renderContent(data));
+    currPage = e.target.value;
+    loadData(currSection, currPage).then((data) => renderContent(data));
 });
-
-// readLaterButton.addEventListener('click', (e) => {
-//     console.log('button clicked');
-// });
