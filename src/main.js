242b9c64b3a9c2bad16e5025fe4cab2078ea03f8
Damia Dembicki <nikeid1235@gmail.com>
2021-03-22 18:02:56 +0100

    Added news section selection

--- src/main.js
+++ src/main.js
@@ -1,39 +1,40 @@
 import './styles/main.css';
 
 const newsListElement = document.querySelector('.newsList');
+const sectionSelectElement = document.querySelector('#sectionSelect');
 
 // Please use https://open-platform.theguardian.com/documentation/
 
-const loadData = async () => {
+const loadData = async (section) => {
     const API_KEY = '7eb760ef-b13f-409e-a7d0-0a7226c8356c';
     const URL = 'https://content.guardianapis.com';
-    const section = 'search';
+    const slug = section ? section : 'search';
 
-    const response = await fetch(`${URL}/${section}?api-key=${API_KEY}`);
+    const response = await fetch(`${URL}/${slug}?api-key=${API_KEY}`);
     const json = await response.json();
     console.log(json);
 
     const news = json.response.results
         .map(
-            (item) => `
-    <li>
-    <article class="news">
-      <header>
-        <h3>${item.webTitle}</h3>
-      </header>
-      <section class="newsDetails">
-        <ul>
-          <li><strong>Section Name:</strong> ${item.sectionName}</li>
-          <li><strong>Publication Date:</strong>${item.webPublicationDate}</li>
-        </ul>
-      </section>
-      <section class="newsActions">
-        <a href="${item.webUrl}" class="button">Full article</a>
-        <button class="button button-outline">Read Later</button>
-      </section>
-    </article>
-  </li>
-    `
+            ({ webTitle, sectionName, webPublicationDate, webUrl }) => `
+                <li>
+                    <article class="news">
+                    <header>
+                        <h3>${webTitle}</h3>
+                    </header>
+                    <section class="newsDetails">
+                        <ul>
+                        <li><strong>Section Name:</strong> ${sectionName}</li>
+                        <li><strong>Publication Date:</strong>${webPublicationDate}</li>
+                        </ul>
+                    </section>
+                    <section class="newsActions">
+                        <a href="${webUrl}" target="_blank" class="button">Full article</a>
+                        <button class="button button-outline">Read Later</button>
+                    </section>
+                    </article>
+                </li>
+                `
         )
         .join('');
 
@@ -42,5 +43,8 @@ const loadData = async () => {
 
 loadData();
 
-// console.log(news);
-// const newsContent = loadNews(news);
+sectionSelectElement.addEventListener('change', (e) => {
+    let section = e.target.value.toLowerCase();
+    if (section === 'all') section = 'search';
+    loadData(section);
+});
