7d801d6a4cd36040bd710fc8e5f16c749ef0317b
Damia Dembicki <nikeid1235@gmail.com>
2021-03-22 18:42:23 +0100

    Fix date format

--- src/main.js
+++ src/main.js
@@ -10,20 +10,21 @@ let currSection = 'search';
 let currPage = 1;
 
 const loadData = async (section, page) => {
-  const API_KEY = '7eb760ef-b13f-409e-a7d0-0a7226c8356c';
-  const URL = 'https://content.guardianapis.com';
-  const slug = section ? section : 'search';
-  const pageNr = page ? page : 1;
+    const API_KEY = '7eb760ef-b13f-409e-a7d0-0a7226c8356c';
+    const URL = 'https://content.guardianapis.com';
+    const slug = section ? section : 'search';
+    const pageNr = page ? page : 1;
 
-  const response = await fetch(
-    `${URL}/${slug}?api-key=${API_KEY}&page=${pageNr}`
-  );
-  const json = await response.json();
-  console.log(json);
+    const response = await fetch(
+        `${URL}/${slug}?api-key=${API_KEY}&page=${pageNr}`
+    );
+    const json = await response.json();
+    console.log(json);
 
-  const news = json.response.results
-    .map(
-      ({ webTitle, sectionName, webPublicationDate, webUrl }) => `
+    const news = json.response.results
+        .map(({ webTitle, sectionName, webPublicationDate, webUrl }) => {
+            const date = new Date(webPublicationDate).toLocaleDateString('pl-PL');
+            return `
                 <li>
                     <article class="news">
                     <header>
@@ -32,7 +33,7 @@ const loadData = async (section, page) => {
                     <section class="newsDetails">
                         <ul>
                         <li><strong>Section Name:</strong> ${sectionName}</li>
-                        <li><strong>Publication Date:</strong>${webPublicationDate}</li>
+                        <li><strong>Publication Date:</strong> ${date}</li>
                         </ul>
                     </section>
                     <section class="newsActions">
@@ -41,23 +42,23 @@ const loadData = async (section, page) => {
                     </section>
                     </article>
                 </li>
-                `
-    )
-    .join('');
+                `;
+        })
+        .join('');
 
-  newsListElement.innerHTML = news;
+    newsListElement.innerHTML = news;
 };
 
 loadData(currSection);
 
 sectionSelectElement.addEventListener('change', (e) => {
-  currSection = e.target.value.toLowerCase();
-  if (currSection === 'all') currSection = 'search';
-  loadData(currSection);
+    currSection = e.target.value.toLowerCase();
+    if (currSection === 'all') currSection = 'search';
+    loadData(currSection);
 });
 
 activePageSelectElement.addEventListener('change', (e) => {
-  currPage = e.target.value;
-  console.log(currPage);
-  loadData(currSection, currPage);
+    currPage = e.target.value;
+    console.log(currPage);
+    loadData(currSection, currPage);
 });
