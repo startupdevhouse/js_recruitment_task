abd7b1497f30e89ef3219220beb8faf2544ba0ea
Damia Dembicki <nikeid1235@gmail.com>
2021-03-22 19:58:42 +0100

    Added active page proper amount rendering

--- src/main.js
+++ src/main.js
@@ -12,26 +12,28 @@ let currPage = 1;
 
 const today = new Date(); //today's date
 const startDate = new Date(new Date().setDate(today.getDate() - 30))
-    .toISOString()
-    .slice(0, 10); //30 days ago
+  .toISOString()
+  .slice(0, 10); //30 days ago
 const endDate = today.toISOString().slice(0, 10);
 
 const loadData = async (section, page) => {
-    const API_KEY = '7eb760ef-b13f-409e-a7d0-0a7226c8356c';
-    const URL = 'https://content.guardianapis.com';
-    const slug = section ? section : 'search';
-    const pageNr = page ? page : 1;
+  const API_KEY = '7eb760ef-b13f-409e-a7d0-0a7226c8356c';
+  const URL = 'https://content.guardianapis.com';
+  const slug = section ? section : 'search';
+  const pageNr = page ? page : 1;
 
-    const response = await fetch(
-        `${URL}/${slug}?api-key=${API_KEY}&page=${pageNr}&from-date=${startDate}&to-date=${endDate}`
-    );
-    const json = await response.json();
-    console.log(json);
+  const response = await fetch(
+    `${URL}/${slug}?api-key=${API_KEY}&page=${pageNr}&from-date=${startDate}&to-date=${endDate}`
+  );
+  const json = await response.json();
+  console.log(json);
 
-    const news = json.response.results
-        .map(({ webTitle, sectionName, webPublicationDate, webUrl }) => {
-            const date = new Date(webPublicationDate).toLocaleDateString('pl-PL');
-            return `
+  const PageAmount = json.response.pages;
+
+  const news = json.response.results
+    .map(({ webTitle, sectionName, webPublicationDate, webUrl }) => {
+      const date = new Date(webPublicationDate).toLocaleDateString('pl-PL');
+      return `
                 <li>
                     <article class="news">
                     <header>
@@ -50,29 +52,31 @@ const loadData = async (section, page) => {
                     </article>
                 </li>
                 `;
-        })
-        .join('');
+    })
+    .join('');
 
-    // const pages = [];
-    // for (let i = 0; i < json.response.pages; i++) {
-    //     pages.push(`<option value="${page}">${page}</option>`);
-    // }
+  const pages = [{}];
+  for (let i = 1; i <= PageAmount; i++) {
+    pages.push(`<option value="${i}">${i}</option>`);
+  }
+  console.log(pages);
 
-    // activePageSelectElement.innerHTML = pages;
-    newsListElement.innerHTML = news;
+  activePageSelectElement.innerHTML = pages;
+  activePageSelectElement.value = currPage;
+  newsListElement.innerHTML = news;
 };
 
 loadData(currSection);
 
 //event listeners
 sectionSelectElement.addEventListener('change', (e) => {
-    currSection = e.target.value.toLowerCase();
-    if (currSection === 'all') currSection = 'search';
-    loadData(currSection);
+  currSection = e.target.value.toLowerCase();
+  if (currSection === 'all') currSection = 'search';
+  currPage = 1;
+  loadData(currSection, currPage);
 });
 
 activePageSelectElement.addEventListener('change', (e) => {
-    currPage = e.target.value;
-    console.log(currPage);
-    loadData(currSection, currPage);
+  currPage = e.target.value;
+  loadData(currSection, currPage);
 });
