eedb60b7ecad431cceb45a6cfd4d88e0190f8582
Damia Dembicki <nikeid1235@gmail.com>
2021-03-23 13:55:54 +0100

    Code cleanup & Added fetch error handling

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ const sectionSelectElement = document.querySelector('#sectionSelect');
 const activePageSelectElement = document.querySelector('#activePageSelect');
 const readLaterListElement = document.querySelector('.readLaterList');
 const newsContentSearchElement = document.querySelector('#newsContentSearch');
+const errorElement = document.querySelector('.error');
 
 //values
 let currSearch = newsContentSearchElement.value;
@@ -15,22 +16,22 @@ let currSection = sectionSelectElement.value;
 let currPage = activePageSelectElement.value;
 
 const saveToLocalStorage = (e, items) => {
-    const { id, webTitle, webUrl } = items[e.target.dataset.index];
-    const item = { id, webTitle, webUrl, added: Date.now() };
-    const obj = JSON.stringify(item);
-    localStorage.setItem(id, obj);
-    readFromLocalStorage();
+  const { id, webTitle, webUrl } = items[e.target.dataset.index];
+  const item = { id, webTitle, webUrl, added: Date.now() };
+  const obj = JSON.stringify(item);
+  localStorage.setItem(id, obj);
+  readFromLocalStorage();
 };
 
 const readFromLocalStorage = () => {
-    let values = [],
-        keys = Object.keys(localStorage),
-        i = keys.length;
-    while (i--) values.push(JSON.parse(localStorage.getItem(keys[i])));
-    const render = values
-        ?.sort((a, b) => b.added - a.added)
-        ?.map(
-            ({ id, webTitle, webUrl }) => `
+  let values = [],
+    keys = Object.keys(localStorage),
+    i = keys.length;
+  while (i--) values.push(JSON.parse(localStorage.getItem(keys[i])));
+  const render = values
+    ?.sort((a, b) => b.added - a.added)
+    ?.map(
+      ({ id, webTitle, webUrl }) => `
             <li class="readLaterItem>
                 <h4 class="readLaterItem-title">${webTitle}</h4>
                 <section>
@@ -38,55 +39,60 @@ const readFromLocalStorage = () => {
                 <button class="button button-clear remove-button" data-id="${id}">Remove</button>
                 </section>
             </li> `
-        )
-        .join('');
+    )
+    .join('');
 
-    if (!render) {
-        readLaterListElement.innerHTML =
+  if (!render) {
+    readLaterListElement.innerHTML =
       '<div><p>Nothing on your list yet... <br> Click "Read Later" to add items to your list</div>';
-    } else {
-        readLaterListElement.innerHTML = render;
-    }
-    const removeButtons = document.querySelectorAll('.remove-button');
-    removeButtons.forEach((button) =>
-        button.addEventListener('click', (e) => removeFromLocalStorage(e))
-    );
+  } else {
+    readLaterListElement.innerHTML = render;
+  }
+  const removeButtons = document.querySelectorAll('.remove-button');
+  removeButtons.forEach((button) =>
+    button.addEventListener('click', (e) => removeFromLocalStorage(e))
+  );
 };
 
 const removeFromLocalStorage = (e) => {
-    const { id } = e.target.dataset;
-    localStorage.removeItem(id);
-    readFromLocalStorage();
+  const { id } = e.target.dataset;
+  localStorage.removeItem(id);
+  readFromLocalStorage();
 };
 
 const loadData = async () => {
-    const API_KEY = '7eb760ef-b13f-409e-a7d0-0a7226c8356c';
-    const URL = 'https://content.guardianapis.com';
-    const startDate = new Date(new Date().setDate(new Date().getDate() - 30));
-    const params = {
-        q: currSearch ? currSearch : '',
-        page: currPage ? currPage : 1,
-        'from-date': startDate.toISOString().slice(0, 10),
-        'api-key': API_KEY,
-    };
+  const API_KEY = '7eb760ef-b13f-409e-a7d0-0a7226c8356c';
+  const URL = 'https://content.guardianapis.com';
+  const startDate = new Date(new Date().setDate(new Date().getDate() - 30));
+  const params = {
+    q: currSearch ? currSearch : '',
+    page: currPage ? currPage : 1,
+    'from-date': startDate.toISOString().slice(0, 10),
+    'api-key': API_KEY,
+  };
 
-    if (currSection !== 'all') params['section'] = currSection;
+  if (currSection !== 'all') params['section'] = currSection;
 
-    const response = await fetch(URL + '/search?' + new URLSearchParams(params));
-    const json = await response.json();
-    console.log(json);
-    return json;
+  const resp = await fetch(
+    URL + '/search?' + new URLSearchParams(params)
+  ).catch(() => {
+    errorElement.innerHTML =
+      'Oops, we cannot get the latest news, please try again';
+  });
+  const json = await resp.json();
+  console.log(json);
+  return json;
 };
 
 const renderContent = (json) => {
-    const pages = [{}];
-    const pageAmount = json.response.pages;
-    const items = json.response.results;
+  const pagesArr = [];
+  const { pages } = json.response;
+  const { results } = json.response;
 
-    const news = items
-        ?.map(({ webTitle, sectionName, webPublicationDate, webUrl }, index) => {
-            const date = new Date(webPublicationDate).toLocaleDateString('pl-PL');
-            return `
+  const news = results
+    ?.map(({ webTitle, sectionName, webPublicationDate, webUrl }, index) => {
+      const date = new Date(webPublicationDate).toLocaleDateString('pl-PL');
+      return `
                 <li>
                     <article class="news">
                     <header>
@@ -105,49 +111,53 @@ const renderContent = (json) => {
                     </article>
                 </li>
                 `;
-        })
-        .join('');
+    })
+    .join('');
 
-    //render active page list
-    for (let i = 1; i <= pageAmount; i++) {
-        pages.push(`<option value="${i}">${i}</option>`);
-    }
+  //render active page list
+  for (let i = 1; i <= pages; i++) {
+    pagesArr?.push(`<option value="${i}">${i}</option>`);
+  }
 
-    //render elements
-    activePageSelectElement.innerHTML = pages;
-    activePageSelectElement.value = currPage;
-    newsListElement.innerHTML = news;
+  //render elements
+  activePageSelectElement.innerHTML = pagesArr;
+  activePageSelectElement.value = currPage;
+  newsListElement.innerHTML = news;
 
-    //add functions to read later buttons
-    const saveButtons = document.querySelectorAll('.save-button');
-    saveButtons.forEach((button) =>
-        button.addEventListener('click', (e) => saveToLocalStorage(e, items))
-    );
+  //add functions to read later buttons
+  const saveButtons = document.querySelectorAll('.save-button');
+  saveButtons.forEach((button) =>
+    button.addEventListener('click', (e) => saveToLocalStorage(e, results))
+  );
 };
 
-//initial load data
-loadData().then((data) => renderContent(data));
-readFromLocalStorage();
+const pageLoad = () => {
+  loadData().then((data) => renderContent(data));
+  readFromLocalStorage();
 
-//event listeners
-sectionSelectElement?.addEventListener('change', (e) => {
+  //event listeners
+  sectionSelectElement?.addEventListener('change', (e) => {
     currSection = e.target.value.toLowerCase();
     currPage = 1;
-    console.log(currSearch);
     loadData().then((data) => renderContent(data));
-});
+  });
 
-activePageSelectElement?.addEventListener('change', (e) => {
+  activePageSelectElement?.addEventListener('change', (e) => {
     currPage = e.target.value;
     loadData().then((data) => renderContent(data));
-});
+  });
 
-newsContentSearchElement?.addEventListener('input', (e) => {
+  newsContentSearchElement?.addEventListener('input', (e) => {
     let timeout = null;
     clearTimeout(timeout);
     timeout = setTimeout(() => {
-        currSearch = e.target.value;
-        currPage = 1;
-        loadData().then((data) => renderContent(data));
+      currSearch = e.target.value;
+      currPage = 1;
+      loadData().then((data) => renderContent(data));
     }, 1000);
-});
+  });
+};
+
+//initial load data
+
+pageLoad();
